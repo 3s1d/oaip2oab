bb9c548ac33c57b9fbaee906e26b32ecd0776731
Juergen Eckert <juergen@familie-eckert.de>
2018-08-11 17:09:48 +0200

    scanning directory

--- Src/oaip2oab.cpp
+++ Src/oaip2oab.cpp
@@ -7,11 +7,29 @@
 
 
 #include <iostream>
+#include <vector>
+#include <boost/filesystem.hpp>
 
 #include "aiphandler.hpp"
 #include "xml.hpp"
 
 using namespace std;
+
+struct path_leaf_string
+{
+    std::string operator()(const boost::filesystem::directory_entry& entry) const
+    {
+        return entry.path().leaf().string();
+    }
+};
+
+void read_directory(const std::string& name, vector<string>& v)
+{
+	boost::filesystem::path p(name);
+	boost::filesystem::directory_iterator start(p);
+	boost::filesystem::directory_iterator end;
+	std::transform(start, end, std::back_inserter(v), path_leaf_string());
+}
 
 int main(int argc, char* argv[])
 {
@@ -19,13 +37,30 @@ int main(int argc, char* argv[])
 
 	if (argc != 2)
 	{
-		cerr << "usage: " << argv[0] << " input.aip " << endl;
+		cerr << "usage: " << argv[0] << " aipFolder " << endl;
 		return 1;
 	}
 
+	/* get files */
+	// note: no subfolders supported
+	vector<string> files;
+	read_directory(argv[1], files);
+
 	aip::handler aiphandler;
 	xml::parser < aip::handler > parser(aiphandler);
-	parser.parse(argv[1]);
+
+	/* iterate over all files */
+	for(auto file : files)
+	{
+		/* is aip file */
+		//todo improve check
+		if(file.find(".aip") == std::string::npos)
+			continue;
+		std::cout << "Converting file: " << file << std::endl;
+
+		/* add to aip handler */
+		parser.parse(argv[1]);
+	}
 
 	return 0;
 }
